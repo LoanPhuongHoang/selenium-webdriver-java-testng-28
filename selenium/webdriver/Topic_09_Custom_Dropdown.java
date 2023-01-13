7f54eea375ce4583bbcd27c1cb06784b9950c649
LoanHoang <hoangphuongloan1304@gmail.com>
2023-01-13 12:00:26 +0700

    Topic 27 (Handle Custom Dropdown - Part II)

--- selenium/webdriver/Topic_09_Custom_Dropdown.java
+++ selenium/webdriver/Topic_09_Custom_Dropdown.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -31,41 +32,88 @@ public class Topic_09_Custom_Dropdown {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 	
-	@Test
+	
 	public void TC_01_JQuery() {
 		driver.get("https://jqueryui.com/resources/demos/selectmenu/default.html");
-		//Click to any tag to make dropdown list to expand
-		driver.findElement(By.cssSelector("span#speed-button")).click();
-		//Wait for all element to list
-		//locator for all elements
-		//take the tag contains text
-		explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("ul#speed-menu div[role='option']")));
-		//Put all items to a list
-		List<WebElement> speedDropdownItems = driver.findElements(By.cssSelector("ul#speed-menu div[role='option']"));
-		//Find if it is the item we need
-		for (WebElement tempItem : speedDropdownItems) {
-			String itemText = tempItem.getText();
-			System.out.println(itemText);
-			//verify if text of item is what we want
-			if (itemText.equals("Fast")) {
-				//click to item
-				System.out.println("Click item");
-				tempItem.click();
-				//exit loop
-			}else {
-				System.out.println("Do not click item");
-			}
-		}
+		//Select a speed
+		selectItemDropdown("span#speed-button", "ul#speed-menu div[role='option']", "Slower");
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#speed-button>span.ui-selectmenu-text")).getText(), "Slower");
+		
+		selectItemDropdown("span#speed-button", "ul#speed-menu div[role='option']", "Faster");
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#speed-button>span.ui-selectmenu-text")).getText(), "Faster");
+		
+		selectItemDropdown("span#speed-button", "ul#speed-menu div[role='option']", "Slow");
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#speed-button>span.ui-selectmenu-text")).getText(), "Slow");
+		
+		selectItemDropdown("span#speed-button", "ul#speed-menu div[role='option']", "Fast");
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#speed-button>span.ui-selectmenu-text")).getText(), "Fast");
+		//Select a title
+		selectItemDropdown("span#salutation-button", "ul#salutation-menu div[role='option']", "Prof.");
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#salutation-button>span.ui-selectmenu-text")).getText(), "Prof.");
+				
 	}
 	
-	@Test
-	public void TC_02_() {
+	
+	public void TC_02_ReactJS() {
+		driver.get("https://react.semantic-ui.com/maximize/dropdown-example-selection/");
+		//Select
+		selectItemDropdown("i.dropdown.icon", "span.text", "Stevie Feliciano");
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.divider.text")).getText(), "Stevie Feliciano");
+		
+		selectItemDropdown("i.dropdown.icon", "span.text", "Matt");
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.divider.text")).getText(), "Matt");
+		
+		selectItemDropdown("i.dropdown.icon", "span.text", "Jenny Hess");
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.divider.text")).getText(), "Jenny Hess");
+		
 		
 	}
 	
-	@Test
-	public void TC_03_() {
+	
+	public void TC_03_VueJS() {
+		driver.get("https://mikerodham.github.io/vue-dropdowns/");
+		//Select a speed
+		selectItemDropdown("li.dropdown-toggle", "ul.dropdown-menu a", "Second Option");
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.cssSelector("li.dropdown-toggle")).getText(), "Second Option");
 		
+		selectItemDropdown("li.dropdown-toggle", "ul.dropdown-menu a", "First Option");
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.cssSelector("li.dropdown-toggle")).getText(), "First Option");
+		
+		selectItemDropdown("li.dropdown-toggle", "ul.dropdown-menu a", "Third Option");
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.cssSelector("li.dropdown-toggle")).getText(), "Third Option");
+	}
+	
+	@Test
+	public void TC_04_Editable() {
+		driver.get("https://react.semantic-ui.com/maximize/dropdown-example-search-selection/");
+		//Select a speed
+		enterItemDropdown("input.search", "span.text", "Angola");
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.divider.text")).getText(), "Angola");
+		
+		selectItemDropdown("input.search", "span.text", "Australia");
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.divider.text")).getText(), "Australia");
+		
+		selectItemDropdown("input.search", "span.text", "Belgium");
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.divider.text")).getText(), "Belgium");
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		//driver.quit();
 	}
 	
 	public void sleepInSecond(long timeInSecond) {
@@ -78,8 +126,58 @@ public class Topic_09_Custom_Dropdown {
 		//1000ms = 1s
 	}
 	
-	@AfterClass
-	public void afterClass() {
-		//driver.quit();
+	public void selectItemDropdown(String parentCss, String allItemCss, String expectedTextItem) {
+		driver.findElement(By.cssSelector(parentCss)).click();
+		sleepInSecond(1);
+		//Wait for all element to list
+		//locator for all elements
+		//take the tag contains text
+		explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(allItemCss)));
+		//Put all items to a list
+		List<WebElement> speedDropdownItems = driver.findElements(By.cssSelector(allItemCss));
+		//Find if it is the item we need
+		for (WebElement tempItem : speedDropdownItems) {
+//			String itemText = tempItem.getText();
+//			System.out.println(itemText);
+			//verify if text of item is what we want
+			if (tempItem.getText().trim().equals(expectedTextItem)) {
+				//Print out
+				System.out.println("Click item");
+				sleepInSecond(1);
+				//click to item
+				tempItem.click();
+				//exit loop
+				break;
+			}
+		}
+	}
+	
+	public void enterItemDropdown(String textboxCss, String allItemCss, String expectedTextItem) {
+		//clear
+		driver.findElement(By.cssSelector(textboxCss)).clear();
+		//input expected item- list of matching item is displayed
+		driver.findElement(By.cssSelector(textboxCss)).sendKeys(expectedTextItem);
+		sleepInSecond(1);
+		//Wait for all element to list
+		//locator for all elements
+		//take the tag contains text
+		explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(allItemCss)));
+		//Put all items to a list
+		List<WebElement> speedDropdownItems = driver.findElements(By.cssSelector(allItemCss));
+		//Find if it is the item we need
+		for (WebElement tempItem : speedDropdownItems) {
+//			String itemText = tempItem.getText();
+//			System.out.println(itemText);
+			//verify if text of item is what we want
+			if (tempItem.getText().trim().equals(expectedTextItem)) {
+				//Print out
+//				System.out.println("Click item");
+				sleepInSecond(1);
+				//click to item
+				tempItem.click();
+				//exit loop
+				break;
+			}
+		}
 	}
 }
